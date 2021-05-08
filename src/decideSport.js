4da7ad3345a13a57ed9c99c039a355344484cb60
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-08 23:48:03 +0300

    First message

---
+++ src/decideSport.js
@@ -0,0 +1,18 @@
+import React, { Component}  from 'react'
+
+
+
+class DecideSport extends Component {
+
+    render() {
+        return(
+            <div>
+                    <h1>Decide Sport...</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default DecideSport
