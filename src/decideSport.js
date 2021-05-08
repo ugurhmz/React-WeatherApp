d27320b26b0e7014b499e21547d9af7553bbec44
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-09 00:33:28 +0300

    geolocation & set & this.setState

--- src/decideSport.js
+++ src/decideSport.js
@@ -4,10 +4,31 @@ import React, { Component}  from 'react'
 
 class DecideSport extends Component {
 
+    state = {
+        longitude:0  //Stati al, aşağıad setState ile güncelle
+    }
+
+
     render() {
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position)
+                this.setState({
+                    longitude:position.coords.longitude
+                });
+            },
+            (err) => {
+                console.log(err)
+            }
+        );
+
+
+
         return(
             <div>
                     <h1>Decide Sport...</h1>
+                    <p>longitude : {this.state.longitude}</p>
             </div>
         )
     }
